a04f9b2acd95338900457bcca1fb7f1a9bcc2064
k <151729014+kipper-jack@users.noreply.github.com>
2025-11-14 16:19:16 +0800

    Update github_daily_run.py

--- github_daily_run.py
+++ github_daily_run.py
@@ -17,8 +17,8 @@ from datetime import datetime
 from DrissionPage import ChromiumPage, ChromiumOptions
 
 # 配置参数
-TEL_NUMBER = '19923846354'
-TEL_NAME = '黄薪颖'
+TEL_NUMBER = '18223994316'
+TEL_NAME = '沃得发'
 
 # 进度文件
 PROGRESS_FILE = 'progress.json'
